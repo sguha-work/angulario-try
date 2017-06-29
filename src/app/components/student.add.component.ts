76d2b815a356aaede2e135abc03d261882f7c64c
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2017-06-29 20:06:28 +0530

    student component work begins

--- src/app/components/student.add.component.ts
+++ src/app/components/student.add.component.ts
@@ -5,5 +5,8 @@ import {Component} from '@angular/core';
     templateUrl: "app/components/templates/student.add.template.html",
 })
 export class StudentAdd {
-
+    studentName: String;
+    setStudent(name: String):void {
+        this.studentName = name;
+    }
 }
