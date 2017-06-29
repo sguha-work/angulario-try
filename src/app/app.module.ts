62dd8088de50c63a0a71a1b49b7a56aeea5e7380
Sahasrangshu Guha <sahasrangshu.guha@ericsson.com>
2017-06-29 19:07:29 +0530

    mycomponent1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {MyComponent1} from './components/mycomponent1.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, MyComponent1 ],
+  bootstrap:    [ AppComponent, MyComponent1 ]
 })
 export class AppModule { }
---
+++ src/app/components/mycomponent1.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-component1',
+  templateUrl: 'app/components/templates/mycomponent1.template.html',
+})
+export class MyComponent1  {  }
